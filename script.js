56e3e31d2d1a9a69e30418480073f9aac3c7cc6f
akun-freefire <lutfimifzal03@gmail.com>
2026-09-07 11:46:47 +0700

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-const topbar=document.querySelector(".topbar");const btn=document.querySelector(".menu-btn");btn.addEventListener("click",()=>topbar.classList.toggle("open"));document.querySelectorAll("nav a").forEach(a=>a.addEventListener("click",()=>topbar.classList.remove("open")));
